82d4878b3ccf2216925124729ec85d8020e47385
JingleDjango95 <spencerweber019@gmail.com>
2019-10-10 21:46:07 -0600

    Created min and max variables

--- Number_Wizard_Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to number wizard!");
-        Debug.Log("Pick a number, any number! As long as..");
-        Debug.Log("The highest number allowed is 1000");
-        Debug.Log("And the lowest number allowed is 1");
+        Debug.Log("Pick a number, any number!");
+        Debug.Log("The highest number allowed is: " + max);
+        Debug.Log("And the lowest number allowed is: " + min);
     }
 
     // Update is called once per frame
