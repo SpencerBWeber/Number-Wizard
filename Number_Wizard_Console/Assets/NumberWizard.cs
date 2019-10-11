e0aad98dfd737e573e757fb818773754dd8c0c86
JingleDjango95 <spencerweber019@gmail.com>
2019-10-10 22:49:26 -0600

    Cleaned up code

--- Number_Wizard_Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Assets/NumberWizard.cs
@@ -17,6 +17,7 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("And the lowest number allowed is: " + min);
         Debug.Log("Tell me if your number is higher or lower than: " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max += 1;
     }
 
     // Update is called once per frame
@@ -24,21 +25,19 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is your guess higher or lower than: " + guess);
         }
-
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is your guess higher or lower than: " + guess);
         }
-
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter key was pressed.");
+            Debug.Log("I'm a genius!");
         }
     }
 }
