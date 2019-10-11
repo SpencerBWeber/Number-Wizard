a1298e4e389cbadbbdb1ce49ceb16ca58edad282
JingleDjango95 <spencerweber019@gmail.com>
2019-10-10 23:00:42 -0600

    Encapsulated repeated functions

--- Number_Wizard_Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Assets/NumberWizard.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Pick a number, any number!");
         Debug.Log("The highest number allowed is: " + max);
@@ -26,18 +35,23 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is your guess higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is your guess higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("I'm a genius!");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your guess higher or lower than: " + guess);
     }
 }
