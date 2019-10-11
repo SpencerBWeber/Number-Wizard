8d1e4b5ccb17c0b091983abc6bd582e1226f958b
JingleDjango95 <spencerweber019@gmail.com>
2019-10-10 21:59:19 -0600

    Added input for player

--- Number_Wizard_Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Assets/NumberWizard.cs
@@ -14,11 +14,26 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number, any number!");
         Debug.Log("The highest number allowed is: " + max);
         Debug.Log("And the lowest number allowed is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
     }
 }
