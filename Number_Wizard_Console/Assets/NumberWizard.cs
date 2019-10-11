2b135da6b9db7bb38bbc5ecfb47b377a8319580b
JingleDjango95 <spencerweber019@gmail.com>
2019-10-10 22:37:50 -0600

    Made variables global within class, and updated them within conditionals

--- Number_Wizard_Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Assets/NumberWizard.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
-
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Pick a number, any number!");
         Debug.Log("The highest number allowed is: " + max);
         Debug.Log("And the lowest number allowed is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
@@ -24,11 +25,15 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
